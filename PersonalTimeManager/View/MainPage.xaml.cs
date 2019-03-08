e79c14f869a3450351e8a6178d97ffe04730df43
Glor <peter.troy.shitikov@gamil.com>
2019-03-08 13:12:01 +0300

    add "Menu Item" model add "General" page add "Configuration" page add "Settings" page add "Test" page add navigations menu add customers xaml styles restructing project repository

---
+++ PersonalTimeManager/View/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using PersonalTimeManager.Model;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace PersonalTimeManager.View
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public static MainPage CurrentPage;
+        //public const string CURRENTPAGE_NAME = "Main_";
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            CurrentPage = this;
+            //TitleMenu.Text = CURRENTPAGE_NAME;
+            
+        }
+
+        public List<MenuItem> MenuItems { get; } = new List<MenuItem>
+        {
+            new MenuItem() { Title = "Main", ClassType = typeof(GeneralPage) },
+            new MenuItem() { Title = "Configuration", ClassType = typeof(ConfigurationsPage) },
+            new MenuItem() { Title = "Settings", ClassType = typeof(SettingsPage) },
+            new MenuItem() { Title = "Test view", ClassType = typeof(TestPage) }
+        };
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            MenuControl.ItemsSource = MenuItems;
+            if (Window.Current.Bounds.Width < 640)
+            {
+                MenuControl.SelectedIndex = -1;
+            }
+            else
+            {
+                MenuControl.SelectedIndex = 0;
+            }
+        }
+
+        private void MenuControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox listBox = sender as ListBox;
+            MenuItem item = listBox.SelectedItem as MenuItem;
+            if (item != null)
+            {
+                ContentFrame.Navigate(item.ClassType);
+                if (Window.Current.Bounds.Width < 640)
+                {
+                    Splitter.IsPaneOpen = false;
+                }
+            }
+        }
+    }
+
+    public class MenuItemBindingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            MenuItem item = value as MenuItem;
+            return item.Title;//(MainPage.CurrentPage.MenuItems.IndexOf(item) + 1) + ") " + item.Title;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return true;
+        }
+    }
+}
---
+++ PersonalTimeManager/View/TestPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PersonalTimeManager.View
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class TestPage : Page
+    {
+        public TestPage()
+        {
+            this.InitializeComponent();
+        }
+
+        DispatcherTimer dispatcherTimer;
+        DateTimeOffset startTime;
+        DateTimeOffset lastTime;
+        DateTimeOffset stopTime;
+        int timesTicked = 1;
+        int timesToTick = 10;
+
+        public void DispatcherTimerSetup()
+        {
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            //IsEnabled defaults to false
+            TimerLog.Text += "dispatcherTimer.IsEnabled = " + dispatcherTimer.IsEnabled + "\n";
+            startTime = DateTimeOffset.Now;
+            lastTime = startTime;
+            TimerLog.Text += "Calling dispatcherTimer.Start()\n";
+            dispatcherTimer.Start();
+            //IsEnabled should now be true after calling start
+            TimerLog.Text += "dispatcherTimer.IsEnabled = " + dispatcherTimer.IsEnabled + "\n";
+        }
+
+        void dispatcherTimer_Tick(object sender, object e)
+        {
+            DateTimeOffset time = DateTimeOffset.Now;
+            TimeSpan span = time - lastTime;
+            lastTime = time;
+            //Time since last tick should be very very close to Interval
+            TimerLog.Text += timesTicked + "\t time since last tick: " + span.ToString() + "\n";
+            timesTicked++;
+            if (timesTicked > timesToTick)
+            {
+                stopTime = time;
+                TimerLog.Text += "Calling dispatcherTimer.Stop()\n";
+                dispatcherTimer.Stop();
+                //IsEnabled should now be false after calling stop
+                TimerLog.Text += "dispatcherTimer.IsEnabled = " + dispatcherTimer.IsEnabled + "\n";
+                span = stopTime - startTime;
+                TimerLog.Text += "Total Time Start-Stop: " + span.ToString() + "\n";
+            }
+        }
+        private void Page_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimerSetup();
+            Timer();
+        }
+
+        private void Timer()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            Time_1.Text = DateTime.Now.ToString("hh:mm:ss");
+        }
+    }
+}
