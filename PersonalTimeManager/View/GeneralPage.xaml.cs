47cfe72dffbb5a2f1644cd28654fcce8b46efbc3
Glor <peter.troy.shitikov@gamil.com>
2019-03-10 02:32:00 +0300

    add func UpDateClockView

--- PersonalTimeManager/View/GeneralPage.xaml.cs
+++ PersonalTimeManager/View/GeneralPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using PersonalTimeManager.Model;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,7 +26,27 @@ namespace PersonalTimeManager.View
         public GeneralPage()
         {
             this.InitializeComponent();
+            Clock.Action(UpDateClokView);
+            Clock.Start();
         }
 
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            Clock.Stop();
+        }
+
+        private void UpDateClokView(object sender, object e)
+        {
+            Year.Text = DateTime.Now.Year.ToString();
+            var month = DateTime.Now.Month.ToString();
+            Month.Text = month.Length == 2 ? month : "0" + month;
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday || DateTime.Now.DayOfWeek == DayOfWeek.Saturday)
+                Week.Foreground = new SolidColorBrush(Windows.UI.Colors.Red); 
+            Week.Text = DateTime.Now.DayOfWeek.ToString().Substring(0, 3).ToUpper();
+            var day = DateTime.Now.Day.ToString();
+            Day.Text = day.Length == 2 ? day : "0" + day;
+            HoursAndMinutes.Text = DateTime.Now.ToString("hh:mm");
+            Seconds.Text = DateTime.Now.ToString("ss");
+        }
     }
 }
